a9a8b637498c88b2b875aa37561333c7d0467b66
Fahad Devnikar <devnikarfahad@gmail.com>
2024-09-08 07:06:00 +0530

    final

--- components/header/nav/Link.tsx
+++ components/header/nav/Link.tsx
@@ -3,9 +3,18 @@ import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { slide, scale } from '../anim';
 
+interface LinkProps {
+    data: {
+      title: string,
+      href: string,
+      index: number
+    },
+    isActive: boolean,
+    setSelectedIndicator: (href: string) => void
+  }
 
 
-export default function Linkjs({data, isActive, setSelectedIndicator}) {
+export default function Linkjs({data, isActive, setSelectedIndicator} : LinkProps) {
   
     const { title, href, index} = data;
   
