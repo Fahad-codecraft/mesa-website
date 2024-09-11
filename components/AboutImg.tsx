e7fc2a805a5bad323a1af5725e829f7f3eceb5ab
Fahad Devnikar <devnikarfahad@gmail.com>
2024-09-11 21:14:22 +0530

    updated

--- components/AboutImg.tsx
+++ components/AboutImg.tsx
@@ -173,7 +173,7 @@ const CoreTeam = () => {
         </div>
 
         <div className="w-full overflow-x-hidden">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
             {officeBearer.map((member, index) => (
               <AboutImgs key={index} {...member} />
             ))}
